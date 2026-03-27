2e49c94181146737580e88dcbd6b529dab5ae11e
Daniel Ayala <danielayala8@gmail.com>
2026-03-27 16:57:50 -0500

    Se agrego la ruta para el index de Proveedores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,8 @@ $routes->post('/clientes/actualizar/(:num)', 'Cliente::actualizarCliente/$1');//
 /**
  * RUTAS PARA PROVEEDORES
 */
-$routes->get('/proveedores', 'Proveedor::index');
+//$routes->get('/proveedores', 'Proveedor::index');
+$routes->get('/proveedores', 'Proveedor::indexAsync');
 $routes->get('/proveedores/registrar', 'Proveedor::create');//Muestra solo el envio del formulario
 $routes->post('/proveedores/guardar', 'Proveedor::registrarProveedor');//Guarda los datos del formulario a la tabla
 $routes->get('/proveedores/eliminar/(:num)', 'Proveedor::eliminar/$1');//Elimina un registro de la tabla
