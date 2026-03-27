4c2e7e10d307dd4c434f734022a3ac617e720252
Daniel Ayala <danielayala8@gmail.com>
2026-03-27 17:16:13 -0500

    Se Agrego la funcion de listar en el index de Proveedores

--- app/Views/Modulos/proveedoresAsync/index.php
+++ app/Views/Modulos/proveedoresAsync/index.php
@@ -23,7 +23,42 @@
 
 
 
+<script>
+    // Referenciamos la tabla donde se mostraran los proveedores
+    const tableProveedores = document.querySelector("#table-proveedores");
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        async function fetchProveedores(){
+            try {
+                const response = await fetch(`<?= base_url('/proveedores/listar') ?>`);
+                const data = await response.json();
 
+                // Limpiar la tabla antes de agregar los nuevos datos
+                tableProveedores.innerHTML = '';
+
+                // Recorrer los proveedores y agregarlos a la tabla
+                data.forEach(proveedor => {
+                    tableProveedores.innerHTML += `
+                        <td>${proveedor.razon_social}</td>
+                        <td>${proveedor.direccion}</td>
+                        <td>${proveedor.ruc}</td>
+                        <td>${proveedor.telefono}</td>
+                        <td>${proveedor.representante}</td>
+                        <td>
+                            <button class="btn btn-sm btn-warning">Editar</button>
+                            <button class="btn btn-sm btn-danger">Eliminar</button>
+                        </td>
+                    `;
+                });    
+
+            } catch (error) {
+                console.error("Error al obtener los proveedores: ", error);
+            }
+        }
+
+        fetchProveedores()
+    });
+</script>
 
 </div>
 <?= $footer ?>
