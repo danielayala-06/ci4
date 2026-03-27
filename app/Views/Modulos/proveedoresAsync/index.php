1576a4b7379211e892a38e80cecda302e41764e0
Daniel Ayala <danielayala8@gmail.com>
2026-03-27 16:58:47 -0500

    Se agrego una UI basica para proveedores

---
+++ app/Views/Modulos/proveedoresAsync/index.php
@@ -0,0 +1,29 @@
+<?= $header ?>
+<div class="row">
+    <div class="col-md-12">
+        <h5>Gestion de Proveedores</h5>
+    </div>
+    <table class="table table-sm mt-3">
+        <thead>
+            <tr>
+                <th>Razon Social</th>
+                <th>Direccion</th>
+                <th>RUC</th>
+                <th>Telefono</th>
+                <th>Representante</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <!-- Se generara de forma asincrona -->
+        <tbody id="table-proveedores">
+
+        </tbody>
+    </table>
+
+
+
+
+
+
+</div>
+<?= $footer ?>
